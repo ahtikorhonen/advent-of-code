bd43c34e993f64f5a152babf42892aa7e78d3268
ahtikorhonen <ahti.korhonen@aalto.fi>
2022-12-18 01:15:12 +0200

    day 6

--- day-6/6.py
+++ day-6/6.py
@@ -1,11 +1,9 @@
 X = [l.strip() for l in open("day-6/input-6.txt")][0]
 
-substrings = [set(X[i: i + 4]) for i in range(len(X))]
-unique_members = [1 if len(l) == 4 else 0 for l in substrings]
+def find_unique(str_len: str) -> int:
+    substrings = [set(X[i: i + str_len]) for i in range(len(X))]
+    unique_members = [1 if len(l) == str_len else 0 for l in substrings]
+    return unique_members.index(1) + str_len
 
-p1 = unique_members.index(1) + 4
-
-substrings2 = [set(X[i: i + 14]) for i in range(len(X))]
-unique_members2 = [1 if len(l) == 14 else 0 for l in substrings2]
-
-p2 = unique_members2.index(1) + 14
+p1 = find_unique(4)
+p2 = find_unique(14)
